d5750399f50e588c57c21e884b23c4c2cc564fca
Krishancode <145490412+Krishancode@users.noreply.github.com>
2023-09-26 15:44:35 -0400

    Add files via upload

---
+++ In-class exercise 3 Creating your first program-Copy1.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[10]:
+
+
+# importing random module
+import random
+# getting numbers from 1 to 49
+inputNumbers =range(1,49)
+# printing 6 random integer
+print("The 6 random integers are:")
+print(random.sample(inputNumbers, 6))
+
+
+# In[7]:
+
+
+
+
+
+# In[11]:
+
+
+# importing random module
+import random
+# getting numbers from 1 to 49
+inputNumbers =range(1,49)
+# printing 6 random integer
+print("The 6 random integers are:")
+print(random.sample(inputNumbers, 6))
+
+
+# In[12]:
+
+
+# importing random module
+import random
+# getting numbers from 1 to 49
+inputNumbers =range(1,49)
+# printing 6 random integer
+print("The 6 random integers are:")
+print(random.sample(inputNumbers, 6))
+
+
+# In[13]:
+
+
+# importing random module
+import random
+# getting numbers from 1 to 49
+inputNumbers =range(1,49)
+# printing 6 random integer
+print("The 6 random integers are:")
+print(random.sample(inputNumbers, 6))
+
+
+# In[14]:
+
+
+# importing random module
+import random
+# getting numbers from 1 to 49
+inputNumbers =range(1,49)
+# printing 6 random integer
+print("The 6 random integers are:")
+print(random.sample(inputNumbers, 6))
+
+
+# In[15]:
+
+
+print("The 6 random integers are:")
+print(random.sample(inputNumbers, 6))
+
